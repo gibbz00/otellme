42234dd3e32cacb081ce1fa54891b12d612cf39f
gibbz00 <gabrielhansson00@gmail.com>
2024-03-02 15:14:37 +0100

    Remove project template remains.

--- crates/ingestor/src/lib.rs
+++ crates/ingestor/src/lib.rs
@@ -5,18 +5,3 @@ pub(crate) use services::*;
 
 mod server;
 pub use server::OtlpIngestor;
-
-/// Temp documentation
-pub fn hello_otellme() -> &'static str {
-    "hello!"
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn temp() {
-        assert_eq!("hello!", hello_otellme())
-    }
-}
