14132f677e95f323caf67d4fef44145e9b8d27a0
gibbz00 <gabrielhansson00@gmail.com>
2024-03-03 20:25:08 +0100

    grpc: Add `empty_request_returns_successful_response` test.

--- crates/ingestor/src/grpc/core.rs
+++ crates/ingestor/src/grpc/core.rs
@@ -21,15 +21,37 @@ impl<M: SignalMessage> GrpcSignalServer<M> {
     pub async fn ingest(&self, request: Request<M::Request>) -> Result<Response<M::Response>, Status> {
         tracing::info!("recieved request!");
 
-        // If the server receives an empty request
-        // (a request that does not carry any telemetry data)
-        // the server SHOULD respond with success.
-        //
-        // https://opentelemetry.io/docs/specs/otlp/#full-success
         if request.get_ref().is_empty() {
             return Ok(Response::new(M::Response::sucessful()));
         }
 
         Ok(Response::new(M::Response::sucessful()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // IMPROVEMENT: somewhat rendant but the other signals can have the respective tests too.
+
+    #[cfg(feature = "logs")]
+    mod logs {
+
+        use opentelemetry_proto::tonic::collector::logs::v1::ExportLogsServiceRequest;
+
+        use super::*;
+
+        // If the server receives an empty request
+        // (a request that does not carry any telemetry data)
+        // the server SHOULD respond with success.
+        //
+        // https://opentelemetry.io/docs/specs/otlp/#full-success
+        #[tokio::test]
+        async fn empty_request_returns_successful_response() {
+            let empty_request = Request::new(ExportLogsServiceRequest { resource_logs: vec![] });
+            let actual_response = GrpcSignalServer::<LogsMessage>::new().ingest(empty_request).await.unwrap();
+            assert_eq!(&<LogsMessage as SignalMessage>::Response::sucessful(), actual_response.get_ref());
+        }
     }
 }
