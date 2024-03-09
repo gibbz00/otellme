c0934f20972e7ca23a9bccf688100c1cc4f1c353
gibbz00 <gabrielhansson00@gmail.com>
2024-03-09 15:51:49 +0100

    ingestor: Extract `grpc::server` module to its own file.

--- crates/ingestor/src/grpc/mod.rs
+++ crates/ingestor/src/grpc/mod.rs
@@ -5,40 +5,5 @@ mod namings;
 
 mod service;
 
+mod server;
 pub(crate) use server::GrpcServer;
-mod server {
-    use std::net::SocketAddr;
-
-    use tonic::transport::server::Router;
-
-    use crate::*;
-
-    pub struct GrpcServer {
-        server: Router,
-        socket_address: SocketAddr,
-    }
-
-    impl OtlpServer for GrpcServer {
-        type ServeError = tonic::transport::Error;
-
-        fn configure(socket_address: impl Into<SocketAddr>) -> Self {
-            let mut server = tonic::transport::Server::builder();
-
-            #[cfg(feature = "logs")]
-            let server = server.add_service(GrpcSignalServer::<LogsMessage>::new());
-            #[cfg(feature = "metrics")]
-            let server = server.add_service(GrpcSignalServer::<MetricsMessage>::new());
-            #[cfg(feature = "traces")]
-            let server = server.add_service(GrpcSignalServer::<TracesMessage>::new());
-
-            Self {
-                server,
-                socket_address: socket_address.into(),
-            }
-        }
-
-        async fn serve(self) -> Result<(), Self::ServeError> {
-            self.server.serve(self.socket_address).await
-        }
-    }
-}
