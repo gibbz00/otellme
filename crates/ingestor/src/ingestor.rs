d04ae0ae1e9029560098c150d99b9316258bc565
gibbz00 <gabrielhansson00@gmail.com>
2024-03-09 09:35:13 +0100

    ingestor: Begin a grpc to http system separation.

--- crates/ingestor/src/ingestor.rs
+++ crates/ingestor/src/ingestor.rs
@@ -5,17 +5,17 @@ use tonic::transport::{Error as TonicError, Server as TonicServer};
 use crate::*;
 
 /// Server implementation of the [OTLP Specification 1.0](https://opentelemetry.io/docs/specs/otlp/).
-/// Recieves OTLP gRPC calls and places the contained singals into the configured storage.
+/// Recieves OTLP calls and places the contained singals into the configured storage.
 pub struct OtlpIngestor;
 
 impl OtlpIngestor {
-    /// Beging building a new ingestor up for configuration before finally calling [`Self::serve`]
+    /// Beging building a new ingestor up for configuration before finally calling [`Self::serve_grpc`]
     pub fn builder() -> Self {
         Self
     }
 
     /// Consume the ingestor and beging serving.
-    pub async fn serve(self, addr: impl Into<SocketAddr>) -> Result<(), TonicError> {
+    pub async fn serve_grpc(self, addr: impl Into<SocketAddr>) -> Result<(), TonicError> {
         let mut server = TonicServer::builder();
 
         #[cfg(feature = "logs")]
--- crates/ingestor/src/main.rs
+++ crates/ingestor/src/main.rs
@@ -1,5 +1,7 @@
 //! Binary for starting the ingestor.
 
+// TEMP: for experimentation, should probably be moved to a `ingestor-cli` or the like
+
 use std::net::SocketAddrV6;
 
 use otellme_ingestor::*;
@@ -9,9 +11,9 @@ use otellme_utils::*;
 async fn main() -> anyhow::Result<()> {
     InternalInstrumentor::init()?;
 
-    // TODO: support port overrides
+    // TODO: support port overrides by config, env, var cli flag, etc.
     let addr = "[::1]:4317".parse::<SocketAddrV6>()?;
-    OtlpIngestor::builder().serve(addr).await?;
+    OtlpIngestor::builder().serve_grpc(addr).await?;
 
     Ok(())
 }
