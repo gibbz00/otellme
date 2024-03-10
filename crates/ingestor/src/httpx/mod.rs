93c6e9553dbeae09b0d4366966b1760197920000
gibbz00 <gabrielhansson00@gmail.com>
2024-03-10 10:04:11 +0100

    ingestor: Extract `httpx::server` to its own file.

--- crates/ingestor/src/httpx/mod.rs
+++ crates/ingestor/src/httpx/mod.rs
@@ -1,33 +1,5 @@
 mod signal_paths;
 pub(crate) use signal_paths::HttpSignalPath;
 
+mod server;
 pub(crate) use server::HttpServer;
-mod server {
-    use std::net::SocketAddr;
-
-    use axum::{routing::get, Router};
-    use tokio::net::TcpListener;
-
-    use crate::*;
-
-    pub struct HttpServer {
-        socket_address: SocketAddr,
-        router: Router,
-    }
-
-    impl OtlpServer for HttpServer {
-        fn configure(socket_address: impl Into<std::net::SocketAddr>) -> Self {
-            let router = Router::new().route("/temp", get(|| async { "temp" }));
-
-            Self {
-                socket_address: socket_address.into(),
-                router,
-            }
-        }
-
-        async fn serve(self) -> anyhow::Result<()> {
-            let listener = TcpListener::bind(self.socket_address).await?;
-            axum::serve(listener, self.router).await.map_err(Into::into)
-        }
-    }
-}
