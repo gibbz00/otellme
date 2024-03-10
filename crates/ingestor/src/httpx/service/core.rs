85bc5c6ebd3ab154ddd7a1fd2fc5fce24450c2c5
gibbz00 <gabrielhansson00@gmail.com>
2024-03-10 11:50:24 +0100

    ingestor: Begin separating HTTP request handler.

--- crates/ingestor/src/httpx/service/core.rs
+++ crates/ingestor/src/httpx/service/core.rs
@@ -1,6 +1,10 @@
 use std::marker::PhantomData;
 
-use axum::Router;
+use axum::{
+    extract::Request,
+    response::{IntoResponse, Response},
+    Router,
+};
 
 use crate::*;
 
@@ -9,10 +13,14 @@ pub struct HttpService<M: SignalMessage> {
 }
 
 impl<M: SignalMessage> HttpService<M> {
+    async fn handler(_request: Request) -> Response {
+        "temp".into_response()
+    }
+
     pub fn add_to_router(router: Router) -> Router
     where
         M: HttpSignalPath,
     {
-        router.route(M::DEFAULT_HTTP_PATH, axum::routing::post(|| async { "temp" }))
+        router.route(M::DEFAULT_HTTP_PATH, axum::routing::post(Self::handler))
     }
 }
