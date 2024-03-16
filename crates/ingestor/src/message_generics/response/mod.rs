536e9585f9040f3311ca7dc55c1cc98793af397f
gibbz00 <gabrielhansson00@gmail.com>
2024-03-16 10:26:33 +0100

    Add `PartialSuccessResponse`.

--- crates/ingestor/src/message_generics/response/mod.rs
+++ crates/ingestor/src/message_generics/response/mod.rs
@@ -1,2 +1,5 @@
 mod success;
 pub(crate) use success::SuccessResponse;
+
+mod partial_success;
+pub(crate) use partial_success::PartialSuccessResponse;
---
+++ crates/ingestor/src/message_generics/response/partial_success.rs
@@ -0,0 +1,66 @@
+use crate::*;
+
+// TEMP: ?
+#[allow(dead_code)]
+pub trait PartialSuccessResponse: SealedResponse {
+    /// `rejected_count` signifies the number of spans, data points or log records rejected.
+    /// `reason` SHOULD be populated with a human-readable error message in English.
+    //
+    // NOTE: spec says i64 but we limit ourselves to u32 as
+    // a means of preventing non-sensical negative values.
+    fn partial_success(rejected_count: u32, reason: Option<String>) -> Self;
+}
+
+#[cfg(feature = "logs")]
+mod logs {
+    use opentelemetry_proto::tonic::collector::logs::v1::{ExportLogsPartialSuccess, ExportLogsServiceResponse};
+
+    use crate::*;
+
+    impl PartialSuccessResponse for <LogsMessage as SignalMessage>::Response {
+        fn partial_success(rejected_count: u32, reason: Option<String>) -> Self {
+            ExportLogsServiceResponse {
+                partial_success: Some(ExportLogsPartialSuccess {
+                    rejected_log_records: rejected_count.into(),
+                    error_message: reason.unwrap_or_default(),
+                }),
+            }
+        }
+    }
+}
+
+#[cfg(feature = "metrics")]
+mod metrics {
+    use opentelemetry_proto::tonic::collector::metrics::v1::{ExportMetricsPartialSuccess, ExportMetricsServiceResponse};
+
+    use crate::*;
+
+    impl PartialSuccessResponse for <MetricsMessage as SignalMessage>::Response {
+        fn partial_success(rejected_count: u32, reason: Option<String>) -> Self {
+            ExportMetricsServiceResponse {
+                partial_success: Some(ExportMetricsPartialSuccess {
+                    rejected_data_points: rejected_count.into(),
+                    error_message: reason.unwrap_or_default(),
+                }),
+            }
+        }
+    }
+}
+
+#[cfg(feature = "traces")]
+mod traces {
+    use opentelemetry_proto::tonic::collector::trace::v1::{ExportTracePartialSuccess, ExportTraceServiceResponse};
+
+    use crate::*;
+
+    impl PartialSuccessResponse for <TracesMessage as SignalMessage>::Response {
+        fn partial_success(rejected_count: u32, reason: Option<String>) -> Self {
+            ExportTraceServiceResponse {
+                partial_success: Some(ExportTracePartialSuccess {
+                    rejected_spans: rejected_count.into(),
+                    error_message: reason.unwrap_or_default(),
+                }),
+            }
+        }
+    }
+}
